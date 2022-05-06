22608817d7e0298e41c01f64243b9854a58f0de7
Mogreine <north7311@gmail.com>
2022-05-06 23:08:49 +0300

    Wrote collate function and datamodule

--- src/dataset.py
+++ src/dataset.py
@@ -1,11 +1,15 @@
 import itertools
 import re
+import torch
 import numpy as np
+import pytorch_lightning as pl
 
-from transformers import PreTrainedTokenizer
+from torch.nn.utils.rnn import pad_sequence
+from transformers import PreTrainedTokenizer, AutoTokenizer
 from datasets import load_dataset
 from enum import Enum
-from torch.utils.data import Dataset, IterableDataset, get_worker_info
+from typing import Tuple, List, Dict, Optional
+from torch.utils.data import Dataset, IterableDataset, get_worker_info, DataLoader
 
 
 class Languages(Enum):
@@ -28,11 +32,11 @@ class MultiLanguageDataset(IterableDataset):
     def __init__(
         self,
         tokenizer: PreTrainedTokenizer,
-        is_train: bool = True,
         n_sentences: int = 10,
         max_seq_length: int = 512,
         val_dataset_size: int = int(1e5),
-        seed: int = 57
+        is_train: bool = True,
+        seed: int = 57,
     ):
         worker_info = get_worker_info()
         worker_id = worker_info.id if worker_info is not None else 0
@@ -44,8 +48,6 @@ class MultiLanguageDataset(IterableDataset):
         self.val_dataset_size = val_dataset_size
 
         self.tokenizer = tokenizer
-        self.bos_token_id = tokenizer.cls_token_id
-        self.eos_token_id = tokenizer.sep_token_id
 
         datasets_raw = [load_dataset("wikiann", lang.value) for lang in Languages]
         datasets_raw = [ds["train"] if is_train else ds["validation"] for ds in datasets_raw]
@@ -92,12 +94,85 @@ class MultiLanguageDataset(IterableDataset):
         languages = languages[permutation]
         sentences = sentences[permutation]
 
-        labels = [[self._lang_ids[lang]] * len(self.tokenizer.tokenize(sent)) for sent, lang in zip(sentences, languages)]
+        labels = [
+            [self._lang_ids[lang]] * len(self.tokenizer.tokenize(sent)) for sent, lang in zip(sentences, languages)
+        ]
 
         sample = " ".join(sentences)
-        sample = self.tokenizer.encode(sample, add_special_tokens=False)[: max_seq_length - 2]
-        sample = [self.bos_token_id] + sample + [self.eos_token_id]
+        input_ids, token_type_ids, attention_mask = self.tokenizer(
+            sample, max_length=max_seq_length, truncation=True, padding=False, return_tensors="pt"
+        )
 
-        labels = list(itertools.chain.from_iterable(labels))
+        # -100 labels is for tokenizer special tokens not to calculate loss on them
+        labels = [-100] + list(itertools.chain.from_iterable(labels)) + [-100]
+        labels = torch.tensor(labels)
 
-        return sample, labels
+        return input_ids, token_type_ids, attention_mask, labels
+
+
+class MultiLangCollate:
+    def __call__(self, batch: Tuple[torch.Tensor, ...]) -> Dict[str, torch.Tensor]:
+        input_ids, token_type_ids, attention_mask, labels = list(zip(*batch))
+        input_ids, token_type_ids, attention_mask = [
+            pad_sequence(t).transpose(0, 1) for t in [input_ids, token_type_ids, attention_mask]
+        ]
+        # -100 not to calculate loss on padding tokens
+        labels = pad_sequence(labels, padding_value=-100)
+
+        return {
+            "input_ids": input_ids,
+            "token_type_ids": token_type_ids,
+            "attention_mask": attention_mask,
+            "labels": labels,
+        }
+
+
+class MultiLanguageDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        n_sentences: int = 10,
+        max_seq_length: int = 512,
+        val_dataset_size: int = int(1e5),
+        batch_size: int = 8,
+        num_workers: int = 0,
+        seed: int = 57,
+    ):
+        super().__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
+        self.n_sentences = n_sentences
+        self.max_seq_length = max_seq_length
+        self.val_dataset_size = val_dataset_size
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.seed = seed
+
+        self.collate_fn = MultiLangCollate()
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        self.ds_train = MultiLanguageDataset(
+            self.tokenizer, self.n_sentences, self.max_seq_length, self.val_dataset_size, True, seed=self.seed
+        )
+
+        self.ds_val = MultiLanguageDataset(
+            self.tokenizer, self.n_sentences, self.max_seq_length, self.val_dataset_size, False, seed=self.seed
+        )
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.ds_train,
+            batch_size=self.batch_size,
+            collate_fn=self.collate_fn,
+            num_workers=self.num_workers,
+            shuffle=False,
+            pin_memory=True,
+        )
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.ds_val,
+            batch_size=self.batch_size,
+            collate_fn=self.collate_fn,
+            num_workers=self.num_workers,
+            shuffle=False,
+            pin_memory=True,
+        )
